4427433370d185a3631ba7ca520a8b2ae5e0caf5
pavakta <pavakta@myseneca.ca>
2020-05-15 21:13:15 -0400

    Edits to intro.py

---
+++ README.md
@@ -0,0 +1,10 @@
+## This Repository will contain all the Python files from the udemy program.
+
+### [intro.py](https://github.com/prayvakta/udemy-python/blob/master/intro.py "intro.py")
+
+This is the first Python3 file. Used for test if Python is properly installed.
+
+#### Contents - 
+1. Variable types
+2. Data types
+3. Getting help
--- intro.py
+++ intro.py
@@ -1,2 +1,35 @@
 import datetime
 print('The date and time right now is', datetime.datetime.now())
+
+### Variable types -
+
+# integer
+num = 10 
+
+# float
+float_num = 10.5
+
+# string
+word = "Hello!"
+
+# boolean
+is_available = True
+
+# NoneType - used when we don't have the value yet
+answer = None
+
+### Data types - 
+
+# dictionary (object in other languages)
+data = {'name': 'Pray Vakta', 'id': '001', 'interests': ['problem solving', 'data analysis']}
+
+# list (arrays in other languages)
+list_data = [1, "a", {'item_3': 'index 3'}, [2, 'b']]
+
+# tuple (basically immutable array)
+color = (255, 255, 0, 'yellow')
+
+#####################   GETTING HELP    ####################
+# use `dir()` to look at all the available properties and methods of an object even built-in properties
+
+# use `help()` to look for how a property or method works
